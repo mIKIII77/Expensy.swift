4e1fcbf533a785c11d5360ed0bcceb7a5d9daaca
mIKIII77 <mikhail.karapetyan@uha.fr>
2023-10-12 20:10:00 +0200

    Initial Commit

---
+++ Expensy/ExpensyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensyApp.swift
+//  Expensy
+//
+//  Created by Mikhail Karapetyan on 12/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpensyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
